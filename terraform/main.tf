3f44eba81e6379289a6f4448b8ac29edefe0de6c
Felipe Costa <ant.felipecosta@gmail.com>
2024-08-08 11:42:43 -0300

    backend terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-backend-deepesg"
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
